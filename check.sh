3050d86dcf38b1cc9b053b07091c86023964e16a
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-01-14 09:59:40 +0000

    One more update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- check.sh
+++ check.sh
@@ -3,9 +3,9 @@ for s in $(cat ./leaked.secret)
 do
   echo "checking for $s"
   echo "checking for $s" >> ../check.log.secret
-  find.sh . '*' "$s" > result.secret
-  cat result.secret >> ../check.log.secret
+  find.sh . '*' "$s" > ../result.secret
+  cat ../result.secret >> ../check.log.secret
 done
-rm result.secret
+rm ../result.secret
 mv ../check.log.secret .
-vim check.log.secret
+vim ./check.log.secret
