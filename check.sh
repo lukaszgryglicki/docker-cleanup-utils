3048312b71de53d593c0127b08b90aee022e99d0
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-01-14 09:47:10 +0000

    Script to check for secrets
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ check.sh
@@ -0,0 +1,10 @@
+> check.log.secret
+for s in $(cat ./leaked.secret)
+do
+  echo "checking for $s"
+  echo "checking for $s" >> check.log.secret
+  find.sh . '*' "$s" > result.secret
+  cat result.secret >> check.log.secret
+done
+rm result.secret
+vim check.log.secret
